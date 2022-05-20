05a9d397a29eae2e0080660a06909650e3c088df
indibagusprasetyo <86178402+indibagusprasetyo@users.noreply.github.com>
2022-05-20 20:28:13 +0700

    Add files via upload

---
+++ latihan/Tahun Kabisat.py
@@ -0,0 +1,11 @@
+#Tahun Kabisat
+#I.S = Pengguna memasukan sebuah tahun
+#F.S = Program menampilkan hasil tahun kabisat / bulan
+
+Tahun = int(input("Tahun"))
+if (tahun mod 4 = 0) and (tahun mod 100 != 0) or (tahun 400 = 0)
+then
+    output("Tahun Kabisat")
+else
+    output("Bukan tahun kabisat")
+endif
