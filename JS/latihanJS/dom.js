b763bd70584605051ab30102382b16277b4f6c7f
indibagusprasetyo <86178402+indibagusprasetyo@users.noreply.github.com>
2022-05-21 10:21:54 +0700

    Add files via upload

---
+++ JS/latihanJS/dom.js
@@ -0,0 +1,12 @@
+// javascript object
+const user = {
+    firstName : "Optimum",
+    lastName : "Pride",
+    age : 1000000,
+    isRobot : false,
+    "Home City" : "Majalaya"
+}
+
+console.log(`Halo, nama saya ${user.firstName} ${user.lastName}`);
+console.log(`Umur saya ${user.age} tahun`);
+console.log(`Saya dari ${user["Home City"]}`);
